dcd719631effdd7e2f89d0b88a66f8d2cb702732
Bethyest <168714908+Bethyest@users.noreply.github.com>
2025-09-04 15:07:13 +0300

    Update Script-2.sql

--- Script-2.sql
+++ Script-2.sql
@@ -12,4 +12,5 @@ where author_name not like '% %'
 group by author_name 
 
 select song_name from song
-where song_name like '%Мой%' or song_name like '%My%' or song_name like '%my%'
+where song_name ilike 'Мой %' or song_name ilike '%мой ' or song_name ilike '% мой %'
+or song_name ilike 'my %' or song_name ilike '%my ' or song_name ilike '% my %';
